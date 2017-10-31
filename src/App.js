f9f28e54d2bf23efd85a4040ebc022aea6f4aaff
roman <r.maximov2012@yandex.ru>
2017-10-31 16:55:28 +0300

    теперь пользователь может двигаться

--- src/App.js
+++ src/App.js
@@ -10,26 +10,74 @@ class App extends Component {
 
 
     this.state = {
-      mazeGenerator: new MazeGenerator(10, 10)
+      mazeGenerator: new MazeGenerator(5, 5).run(),
+      coordsUser: [0, 1],
+      win: false
     }
   }
 
 
+  componentDidMount() {
+    document.body.onkeydown = a => {
+      const key = a.keyCode;
+      const { mazeGenerator, coordsUser } = this.state;
+
+      mazeGenerator[coordsUser[0]][coordsUser[1]] = " ";
+
+      if ( a.keyCode === 38 && mazeGenerator[coordsUser[0]-1][coordsUser[1]] !== "=") { //top
+        mazeGenerator[coordsUser[0]-1][coordsUser[1]] = "E";
+
+        this.setState({
+          coordsUser: [coordsUser[0]-1, coordsUser[1]]
+        })
+      }
+
+      if ( a.keyCode === 40 && mazeGenerator[coordsUser[0]+1][coordsUser[1]] !== "=") { //down
+        mazeGenerator[coordsUser[0]+1][coordsUser[1]] = "E";
+
+        this.setState({
+          coordsUser: [coordsUser[0]+1, coordsUser[1]]
+        })
+      }
+
+      if ( a.keyCode === 37 && mazeGenerator[coordsUser[0]][coordsUser[1]-1] !== "=") { //left
+        mazeGenerator[coordsUser[0]][coordsUser[1]-1] = "E";
+
+        this.setState({
+          coordsUser: [coordsUser[0], coordsUser[1]-1]
+        })
+      }
+
+      if ( a.keyCode === 39 && mazeGenerator[coordsUser[0]][coordsUser[1]+1] !== "=") { //right
+        if (mazeGenerator[coordsUser[0]][coordsUser[1]+1] === "G")
+          this.setState({ win: true });
+
+
+        mazeGenerator[coordsUser[0]][coordsUser[1]+1] = "E";
+
+        this.setState({
+          coordsUser: [coordsUser[0], coordsUser[1]+1]
+        })
+      }
+    }
+  }
+
   render() {
-     const maze = this.state.mazeGenerator.run();
+    if(this.state.win)
+      return <div>Игра пройдена, ты молодец!</div>
 
     return (
       <div className="App">
-        {maze.map(i => {
+        {this.state.mazeGenerator.map((i, indexI) => {
           return (
-            <div>
-              {i.map(j => {
-                switch (j) {
-                  case " ": return <span className="space"></span>
-                  case "=": return <span className="wall"></span>
-                  case "E": return <span className="user"></span>
-                  case "G": return <span className="exit"></span>
-                }
+            <div key={indexI}>
+              {i.map((j, indexJ) => {
+                  switch (j) {
+                    case " ": return <span key={indexJ} className="space"></span>
+                    case "=": return <span key={indexJ} className="wall"></span>
+                    case "E": return <span key={indexJ} className="user"></span>
+                    case "G": return <span key={indexJ} className="exit"></span>
+                  }
               })}
             </div>
           );
